85fd0b4a0bb07ba587fe3f1bb3526bd346051c74
Magnus Fernandes <magnusfernandes1295@hotmail.com>
2023-07-08 13:44:11 +0530

    More enums

--- enum.go
+++ enum.go
@@ -77,3 +77,93 @@ type vehicleStatusRegistry struct {
 	New    int16
 	Issued int16
 }
+
+// vehicle events
+var VehicleEvents = newVehicleEventRegistry()
+
+func newVehicleEventRegistry() *vehicleEventRegistry {
+	return &vehicleEventRegistry{
+		OverSpeed:         1,
+		Ignition:          2,
+		TamperAlert:       3,
+		BatteryDisconnect: 4,
+		BatteryInternal:   5,
+		HarshBraking:      6,
+		HarshAcceleration: 7,
+		RashTurning:       8,
+	}
+}
+
+type vehicleEventRegistry struct {
+	OverSpeed         int16
+	Ignition          int16
+	TamperAlert       int16
+	BatteryDisconnect int16
+	BatteryInternal   int16
+	HarshBraking      int16
+	HarshAcceleration int16
+	RashTurning       int16
+}
+
+// packet status
+var PacketStatus = newPacketStatusRegistry()
+
+func newPacketStatusRegistry() *packetStatusRegistry {
+	return &packetStatusRegistry{
+		Live:    "L",
+		History: "H",
+	}
+}
+
+type packetStatusRegistry struct {
+	Live    string
+	History string
+}
+
+// location provider
+var LocationProvider = newLocationProviderRegistry()
+
+func newLocationProviderRegistry() *locationProviderRegistry {
+	return &locationProviderRegistry{
+		Fine:   1,
+		Coarse: 2,
+	}
+}
+
+type locationProviderRegistry struct {
+	Fine   int16
+	Coarse int16
+}
+
+// packet types
+var PacketTypes = newPacketTypeRegistry()
+
+func newPacketTypeRegistry() *packetTypeRegistry {
+	return &packetTypeRegistry{
+		Normal:            "NR",
+		TamperAlert:       "TA",
+		BatteryDisconnect: "BD",
+		BatteryReconnect:  "BR",
+		InternalBattery:   "BL",
+		IgnitionOn:        "IN",
+		IgnitionOff:       "IF",
+		HarshBraking:      "HB",
+		HarshAcceleration: "HA",
+		EmergencyAlert:    "EA",
+		RashTurning:       "RT",
+	}
+}
+
+type packetTypeRegistry struct {
+	Normal            string
+	TamperAlert       string
+	BatteryDisconnect string
+	BatteryReconnect  string
+	InternalBattery   string
+	IgnitionOn        string
+	IgnitionOff       string
+	HarshBraking      string
+	HarshAcceleration string
+	EmergencyAlert    string
+	RashTurning       string
+}
